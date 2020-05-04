195e20f394b937aaf3015c303dc415635a6007b2
Daniel Olszowy (Progeno) <ProgenoDev@gmail.com>
2020-05-04 16:21:01 +0200

    test requests lib and beautiful lib

--- scrap_data.py
+++ scrap_data.py
@@ -1,1 +1,14 @@
-f1 = open()
+#import requests
+
+#r = requests.get("https://ssimeonoff.github.io/cards-list")
+#print(r.text)
+
+
+import requests
+from bs4 import BeautifulSoup
+url = 'https://ssimeonoff.github.io/cards-list'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "html5lib")
+print("1")
+# pierwszym argumentem konstruktora klasy BeautifulSoup jest zawartośc dokumentu HTML
+# drugim parametrem jest parser i nad nim się chwilę zatrzymamy
